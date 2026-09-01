543a213f3e68434864c11ac94d1c051bea33ce2f
tier940 <tier940@protonmail.com>
2026-09-01 12:03:22 +0900

    fix: rename migration file to 000002 sequence number

--- migration/src/lib.rs
+++ migration/src/lib.rs
@@ -6,7 +6,7 @@ mod helpers;
 mod m20260829_000000_initial_schema;
 mod m20260829_000001_redeclare_embedding_width;
 mod m20260831_083204_tenant_embedding_defaults;
-mod m20260901_024302_add_reindex_embeddings_to_audit_log_action_check;
+mod m20260901_000002_add_reindex_embeddings_to_audit_log_action_check;
 pub struct Migrator;
 
 #[async_trait::async_trait]
@@ -16,7 +16,7 @@ impl MigratorTrait for Migrator {
             Box::new(m20260829_000000_initial_schema::Migration),
             Box::new(m20260829_000001_redeclare_embedding_width::Migration),
             Box::new(m20260831_083204_tenant_embedding_defaults::Migration),
-            Box::new(m20260901_024302_add_reindex_embeddings_to_audit_log_action_check::Migration),
+            Box::new(m20260901_000002_add_reindex_embeddings_to_audit_log_action_check::Migration),
             // inject-above (do not remove this comment)
         ]
     }
